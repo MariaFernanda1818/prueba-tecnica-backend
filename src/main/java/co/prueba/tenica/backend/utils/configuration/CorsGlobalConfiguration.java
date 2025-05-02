43f5771806a086a10cbd0d93f66cbfb28319400f
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2025-05-02 03:01:21 -0500

    Ajuste prueba de https

---
+++ src/main/java/co/prueba/tenica/backend/utils/configuration/CorsGlobalConfiguration.java
@@ -0,0 +1,22 @@
+package co.prueba.tenica.backend.utils.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfiguration {
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // o restringe según tu dominio
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
+
